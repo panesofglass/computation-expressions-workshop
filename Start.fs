0d4893bdeac402f0f00125624883bfcb96240283
Ryan Riley <ryanriley@live.com>
2018-08-15 08:59:29 -0500

    Add Start.fs

---
+++ Start.fs
@@ -0,0 +1,30 @@
+module Start
+
+open Expecto
+
+type TraceBuilder() =
+    member __.Return(value) = value
+    member __.Bind(m, f) =
+        printfn "%A %A" m f
+        f m
+
+let trace = TraceBuilder()
+
+[<Tests>]
+let tests =
+    testList "traces" [
+        test "TraceBuilder returns value" {
+            let expected = 1
+            let actual = trace { return expected }
+            Expect.equal actual expected "Actual should match expected"
+        }
+
+        test "TraceBuilder performs a side effect" {
+            let expected = 1
+            let actual = trace {
+                let! result = expected + expected
+                return result
+            }
+            Expect.equal actual (expected + expected) "Actual should match expected + expected"
+        }
+    ]
